7c08b071d42418aefd769d9ed7587883c5fc5e17
Gfavg <godsfavor.gabriel@gmail.com>
2023-05-23 14:07:23 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of new dog
+ * @age: Age of new dog
+ * @owner: Owner of new dog
+ *
+ * Return: Pointer to the newly created struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+};
